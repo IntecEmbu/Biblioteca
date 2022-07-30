abed18766cb89dc287e2b3b9dd75e5f1e2244607
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-29 21:23:37 -0300

    adicionando nome das tabelas

--- back-end/src/services/cataloguingService.js
+++ back-end/src/services/cataloguingService.js
@@ -6,7 +6,7 @@ async function insertBook(data){
     const {title, edition, isbn, year, category, cdd, idiom} = data
 
     // Depois adicionar os nomes corretos das colunas
-    const sql = 'INSERT INTO tbl_book (fields) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    const sql = 'INSERT INTO tbl_book (title, edition, isbn, realease_year, category, cdd, idiom) VALUES (?, ?, ?, ?, ?, ?, ?)'
 
     const values = [title, edition, isbn, year, category, cdd, idiom]
 
