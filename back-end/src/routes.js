83f2afaf9754e57b18574c7a77b49c682e433bbc
pedrofnseca <pedropereira.pp264@gmail.com>
2022-07-29 21:16:52 -0300

    invertendo ordem de notFound

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -3,12 +3,13 @@ import cataloguingController from './controllers/cataloguingController.js'
 
 const router = express.Router()
 
+
+router.use('/cataloguin', cataloguingController)
+
 router.use('/*', (req, res) => {
     res.status(404).json({
         error: 'Not found'
     })
 })
-
-router.use('/cataloguin', cataloguingController)
 
 export default router
